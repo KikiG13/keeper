2a7d41a7662b6c8a5aa250fd0943558c68c7fdcd
Kian G <kian.gormley13@aol.com>
2022-11-18 11:48:22 -0500

    map through notes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,21 @@ import React from 'react';
 import Footing from './Footer';
 import Heading from './Header'
 import Note from './Note'
+import notes from '../notes.js'
 
 
 function App () {
   return <div>
   <Heading />
-  <Note />
+  <div>
+  {notes.map(note => (
+    <Note 
+      key={note.key}
+      title={note.title}
+      content={note.content}
+    />
+  ))}
+  </div>
   <Footing />
   </div>
 }
